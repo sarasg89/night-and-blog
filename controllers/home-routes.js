70171f105d866d46821010d05b265b7947bc7a92
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-04-01 19:09:33 -0400

    added code the handlebars filesfor rendering blog posts in the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,31 @@
 const router = require('express').Router();
+const { BlogPost } = require('../models');
+
+// GET all the blog posts for the homepage
+router.get('/', async (req, res) => {
+    try {
+        const dbBlogData = await BlogPost.findAll();
+        const posts = dbBlogData.map((post) =>
+            post.get({ plain: true })
+        );
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+// Login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
 
 module.exports = router;
